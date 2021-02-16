4026b5a4482bb1a6f7d05cb943c05facb1fdeffa
AB <austinbuhler@gmail.com>
2021-02-16 16:41:25 -0600

    Added bootstrap and modal

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -3,4 +3,11 @@ class LeadsController < ApplicationController
   def index
     @leads = Lead.all
   end
+
+  def new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
