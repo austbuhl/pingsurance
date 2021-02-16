9c6bdf81c5c5b40d762c23f7ec7c257822a1bcb7
AB <austinbuhler@gmail.com>
2021-02-16 17:32:16 -0600

    Form submitting properly and creating

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -11,4 +11,21 @@ class LeadsController < ApplicationController
       format.js
     end
   end
+
+  def create
+    @lead = Lead.create(lead_params)
+    if @lead.valid?
+      flash[:message] = "Lead, #{@lead.full_name}, was succesfully created!"
+    else
+      flash[:message] = "Error creating lead, please try again"
+    end
+    
+    redirect_to leads_path
+  end
+
+  private
+  def lead_params
+    params.require(:lead).permit(:first_name, :last_name, :phone_number)
+  end
+
 end
