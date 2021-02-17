e707e6f478e07dd0c6974897978ac1577d98c2f7
AB <austinbuhler@gmail.com>
2021-02-17 09:20:47 -0600

    Twilio setup

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,5 +1,6 @@
 class Lead < ApplicationRecord
   validates :first_name, :last_name, :phone_number, :message, presence: true
+  validates :phone_number, phone: true
 
   def full_name
     self.first_name + ' ' + self.last_name
